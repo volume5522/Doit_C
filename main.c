1907f40c4c2a64d0cd273e71cfc9ab7d493ba825
kimhyunho <volume5522@naver.com>
2023-08-24 21:25:10 +0900

    06_05_06 tutorial

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int data1 = -1;
-    unsigned int data2 = 4294967295;
-    printf("%d, %u, %d, %u", data1, data1, data2, data2);
+    char data = -1;
+    printf("%d, %u",data, data);
 }
