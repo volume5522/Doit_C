6e5ef857207b578b5e77644335abcc456107fb32
KimHyunHo <volume5522@naver.com>
2023-08-26 00:44:13 +0900

    06_05_08 tutorial

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    int data1 = 10;
-    int data2 = 010;
-    int data3 = 0x10;
-
-    printf("%x, %d, %o",data1,data2,data3);
+    float data = 12.34f;
+    printf("%f, %e, %E",data,data,data);
 }
