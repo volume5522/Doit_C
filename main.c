7593573fe84e14be2d68ff8d81f3239fbe54dea5
KimHyunHo <volume5522@naver.com>
2023-08-26 00:56:07 +0900

    07_01_03 tutorial

--- main.c
+++ main.c
@@ -5,10 +5,10 @@ int main() {
     data1 = 5;
     data2 = 3;
 
-    int result1 = data1 + data2;
-    int result2 = data1 * data2;
-    int result3 = data1 / data2;
-    int result4 = data1 % data2;
+    int result1 = data1 > 7;
+    int result2 = data2 <= data1;
+    int result3 = data2 == 7;
+    int result4 = data2 != data1;
 
     printf("result : %d, %d, %d, %d\n",result1,result2,result3,result4);
 }
