0612a9de3aa4f7cf988379edf9e615ff9c1c6999
KimHyunHo <volume5522@naver.com>
2023-08-26 00:54:25 +0900

    07_01_02 tutorial

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 int main() {
     int data1, data2;
     data1 = 5;
-    data2 = data1;
-    printf("data1 = %d, data2 = %d\n",data1, data2);
+    data2 = 3;
+
+    int result1 = data1 + data2;
+    int result2 = data1 * data2;
+    int result3 = data1 / data2;
+    int result4 = data1 % data2;
+
+    printf("result : %d, %d, %d, %d\n",result1,result2,result3,result4);
 }
