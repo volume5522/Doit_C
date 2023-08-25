449ef9108e90d61c370df004a3c1d28fbf922bce
KimHyunHo <volume5522@naver.com>
2023-08-26 01:49:32 +0900

    07_01_04 tutorial

--- main.c
+++ main.c
@@ -5,10 +5,11 @@ int main() {
     data1 = 5;
     data2 = 3;
 
-    int result1 = data1 > 7;
-    int result2 = data2 <= data1;
-    int result3 = data2 == 7;
-    int result4 = data2 != data1;
+    int result1 = 0 || 1;
+    int result2 = 3 && -1;
+    int result3 = data1 == 3 || data2 == 3;
+    int result4 = data1 == 3 && data2 == 3;
+    int result5 = !data1;
 
-    printf("result : %d, %d, %d, %d\n",result1,result2,result3,result4);
+    printf("result : %d, %d, %d, %d, %d\n",result1,result2,result3,result4,result5);
 }
