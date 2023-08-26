ed31f355a739f98593e430cce6d339c0600dedf4
kimhyunho <volume5522@naver.com>
2023-08-26 17:23:49 +0900

    09_03_01 tutorial

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int sum=0;
-    int num=1;
-    do{
-        printf("num(%d) + sum(%d) = ",num, sum);
-        sum = sum + num;
-        printf("%d\n",sum);
-        num++;
-    }while(num<=5);
-    printf("\nResult : num = %d  sum = %d\n",num,sum);
+    int i;
+    for(i=1;i<=9;i++){
+        printf("2 * %d = %d\n",i,2*i);
+    }
 }
 
