b24e229543d53528ea0249168aaa8866c8c45855
kimhyunho <volume5522@naver.com>
2023-08-26 16:28:35 +0900

    08_04_01 tutorial

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int score =92;
-    char grade;
-    if(score>90){
-        grade='A';
-        printf("점수는 %d이고, 등급 %c입니다.\n",score,grade);
+    int year = 2015, month = 12, day = 31;
+    day ++;
+    if(day>31){
+        month++;
+        day=1;
+        if(month>12){
+            year++;
+            month=1;
+        }
     }
-    else{
-        grade='B';
-        printf("점수는 %d이고, 등급 %c입니다.\n",score,grade);
-    }
-    printf("작업종료\n");
+    printf("Date : %d년 %d월 %d일\n",year,month,day);
 }
 
