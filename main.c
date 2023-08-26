0d1c7cef7c8bce49eb2eb6595bb5aef708f2678a
kimhyunho <volume5522@naver.com>
2023-08-26 16:37:25 +0900

    09_01_02 tutorial

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int sum=0,num;
-    for(num=1;num<=5;num++) {
-        printf("num(%d) + sum(%d) =", num, sum);
+    int sum=0;
+    int num=1;
+    for( ; ; ){
+        printf("num(%d) + sum(%d) = ",num, sum);
         sum = sum + num;
         printf("%d\n",sum);
+        num++;
+        if(num>5)break;
     }
-    printf("\nResult : num = %d   sum = %d\n",num,sum);
+    printf("\nResult : num = %d  sum = %d\n",num,sum);
 }
 
