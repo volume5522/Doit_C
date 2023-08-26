0a0b8365dfdd238a256e410c27f9d0a798329443
kimhyunho <volume5522@naver.com>
2023-08-26 16:15:24 +0900

    08_03_02 tutorial

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int data = 5;
-    if(data > 3) printf("data는 3보다 큰 수입니다.\n");
-    else printf("data는 3보다 작거나 같은 수입니다.\n");
-    printf("작업 종료\n");
+    int score =92;
+    char grade;
+    if(score>90){
+        grade='A';
+        printf("점수는 %d이고, 등급 %c입니다.\n",score,grade);
+    }
+    else{
+        grade='B';
+        printf("점수는 %d이고, 등급 %c입니다.\n",score,grade);
+    }
+    printf("작업종료\n");
 }
+
