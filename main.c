bcc18dd5c2aae786b9a22b4fd146987b034cd80d
kimhyunho <volume5522@naver.com>
2023-08-26 16:49:46 +0900

    09_02_02 tutorial

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 int main() {
     int sum=0;
     int num=1;
-    while(num<=5){
+    do{
         printf("num(%d) + sum(%d) = ",num, sum);
         sum = sum + num;
         printf("%d\n",sum);
         num++;
-    }
+    }while(num<=5);
     printf("\nResult : num = %d  sum = %d\n",num,sum);
 }
 
