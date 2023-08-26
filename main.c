4676f1781928ee3d1990365e035b66219f908386
kimhyunho <volume5522@naver.com>
2023-08-26 15:25:32 +0900

    08_02_01 tutorial

--- main.c
+++ main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int data1, data2;
-    data1 = 5;
-    data2 = 3;
-
-    int result1 = 0 || 1;
-    int result2 = 3 && -1;
-    int result3 = data1 == 3 || data2 == 3;
-    int result4 = data1 == 3 && data2 == 3;
-    int result5 = !data1;
-
-    printf("result : %d, %d, %d, %d, %d\n",result1,result2,result3,result4,result5);
+    int data = 5;
+    if(data>3)printf("data는 큰 수입니다.\n");
+    printf("작업종료\n");
 }
