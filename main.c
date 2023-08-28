4a99ba0fc0e91d7b9c23623a0f73ec37a7b4a09c
kimhyunho <volume5522@naver.com>
2023-08-28 18:49:06 +0900

    09_05_03 tutorial

--- main.c
+++ main.c
@@ -2,12 +2,8 @@
 
 void main(){
     int m,n;
-    for(m=5;m<7;m++){
-        for(n=0;n<3;n++){
-            if(m==5&&n==1)break;
-            printf("m(%d) - n(%d)\n",m,n);
-        }
-        if(m==5&&n==1)break;
+    for(m=5;m<8;m++){
+        if(m==6)continue;
+        printf("m(%d)\n",m);
     }
-
 }
