521ab3d36708a9332979b473a4a30763a9e44dbf
kimhyunho <volume5522@naver.com>
2023-08-28 19:38:53 +0900

    11_01_01 tutorial

--- main.c
+++ main.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-unsigned char GetBit(unsigned char dest_data, unsigned char bit_num){
-    unsigned char bit_state = 0;
-    //1바이트 변수라 0~7 자리까지 비트 이동 가능
-    if(bit_num < 8){
-        bit_state = dest_data & (0x01 << bit_num);
-        bit_state = bit_state >> bit_num;
-    }
-    return bit_state;
+int Sum(int data1, int data2)
+{
+    int result = data1 + data2;
+    return result;
 }
 
-
-void main() {
-    unsigned char lamp_state = 0x75;    // 16진수 75 는 2진수로 0111 0101
-    unsigned  char bit_state;
-    int i;
-
-    printf("%X -> ", lamp_state);    //변경 전 값을 16진법으로 출력
-    // 8개의 비트 값을 모두 출력하기 위해 8번 반복함.
-
-    for(i=0;i<8;i++){
-        bit_state = GetBit(lamp_state,7 - i);
-        // 비트 값을 출력
-        printf("%d", bit_state);
-    }
-    printf("\n");
+int main()
+{
+    int result;
+    result = Sum(5,3);
+    printf("5 + 3 = %d\n", result);
 }
