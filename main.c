be53e6d3ce84358492bbf6d1aca7ec063c56e292
kimhyunho <volume5522@naver.com>
2023-08-28 20:29:30 +0900

    12_01_01 tutorial

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-int result;
 
-void Sum(int data1, int data2)
-{
-    int result;
-    result = data1 + data2;
-}
+void main() {
+   short student[20];
 
-void main()
-{
-    Sum(5,3);
-    printf("5 + 3 = %d\n", result);
+   student[1]=10;
+   printf("%d\n",student[1]);
 }
