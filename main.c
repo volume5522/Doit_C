79d926ef20c6c960497359982391b8a681c7e8ba
kimhyunho <volume5522@naver.com>
2023-08-28 19:29:12 +0900

    10_03_03 tutorial

--- main.c
+++ main.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-unsigned char RestBit(unsigned char dest_data, unsigned char bit_num){
-
-        //1바이트 변수라 0~7 자리까지 비트 이동 가능
-        if(bit_num < 8){
-            dest_data = dest_data | (0x01 << bit_num);
-        }
-        return dest_data;
+unsigned char GetBit(unsigned char dest_data, unsigned char bit_num){
+    unsigned char bit_state = 0;
+    //1바이트 변수라 0~7 자리까지 비트 이동 가능
+    if(bit_num < 8){
+        bit_state = dest_data & (0x01 << bit_num);
+        bit_state = bit_state >> bit_num;
     }
+    return bit_state;
+}
 
 
 void main() {
+    unsigned char lamp_state = 0x75;    // 16진수 75 는 2진수로 0111 0101
+    unsigned  char bit_state;
+    int i;
 
-    unsigned char lamp_state = 0x77;    // 16진수 77 는 2진수로 0111 0111
-    printf("%X ->", lamp_state);    //변경 전 값을 출력
-    //  lamp 변수의 3번 비트를 0으로 설정
-    lamp_state = RestBit(lamp_state, 3);
-    printf(" %X \n ",lamp_state);
+    printf("%X -> ", lamp_state);    //변경 전 값을 16진법으로 출력
+    // 8개의 비트 값을 모두 출력하기 위해 8번 반복함.
 
-
+    for(i=0;i<8;i++){
+        bit_state = GetBit(lamp_state,7 - i);
+        // 비트 값을 출력
+        printf("%d", bit_state);
+    }
+    printf("\n");
 }
