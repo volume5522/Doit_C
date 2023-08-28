9f7e86ba15275557e9a7766b450f7db919b41394
kimhyunho <volume5522@naver.com>
2023-08-28 18:43:10 +0900

    09_04_01 tutorial

--- main.c
+++ main.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-int ShowMultiplication(int step) {
-    int i;
-    for(i=1;i<=9;i++)
-        printf("%d * %d = %d\n",step,i,step*i);
-}
-
-
-void main()
-{
-    int m;
-    for(m=1;m<=9;m++)
-        ShowMultiplication(m);
-
+void main(){
+    int m,n;
+    for(m=5;m<7;m++){
+        for(n=0;n<3;n++){
+            printf("m(%d) - n(%d)\n",m,n);
+        }
+    }
 }
