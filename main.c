b1a9fb0f858bc4afa269d91c1ee032590e056c75
kimhyunho <volume5522@naver.com>
2023-08-28 19:23:52 +0900

    10_03_02 tutorial

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ unsigned char RestBit(unsigned char dest_data, unsigned char bit_num){
 
         //1바이트 변수라 0~7 자리까지 비트 이동 가능
         if(bit_num < 8){
-            dest_data = dest_data & ~(0x01 << bit_num);
+            dest_data = dest_data | (0x01 << bit_num);
         }
         return dest_data;
     }
@@ -12,11 +12,11 @@ unsigned char RestBit(unsigned char dest_data, unsigned char bit_num){
 
 void main() {
 
-    unsigned char lamp_state = 0x7F;    // 16진주 7F 는 2진수로 0111 1111
+    unsigned char lamp_state = 0x77;    // 16진수 77 는 2진수로 0111 0111
     printf("%X ->", lamp_state);    //변경 전 값을 출력
     //  lamp 변수의 3번 비트를 0으로 설정
     lamp_state = RestBit(lamp_state, 3);
-    printf("%X \n ",lamp_state);
+    printf(" %X \n ",lamp_state);
 
 
 }
