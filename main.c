2a1dadf57365931bda8d7c2f3e6c8055d8c87765
kimhyunho <volume5522@naver.com>
2023-08-28 20:04:16 +0900

    11_02_01 tutorial

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+int result;
 
 int Sum(int data1, int data2)
 {
-    int result = data1 + data2;
-    return result;
+    result = data1 + data2;
 }
 
-int main()
+void main()
 {
-    int result;
     result = Sum(5,3);
     printf("5 + 3 = %d\n", result);
 }
