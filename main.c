090ba0a51299a5a16ba80328104dc546b116c5a5
kimhyunho <volume5522@naver.com>
2023-08-29 22:14:36 +0900

    12_01_05 tutorial

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 void main() {
-   short student[20]={0,};
+    char data[5] = {1, 2, 3, 4, 5};
+    int result = 0, i;
 
-   student[1]=10;
-   printf("%d %d\n",student[1],student[2]);
+    for (i = 0; i < 5; i++) {
+        result = result + data[i];
+    }
+    printf("data 배열의 각 요소의 합은 %d입니다.\n", result);
 }
