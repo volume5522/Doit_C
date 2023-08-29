2b058c21f7b33fd3922125ae006ebb28d14c0e19
kimhyunho <volume5522@naver.com>
2023-08-29 22:08:23 +0900

    12_01_03 tutorial

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 void main() {
-   short student[20];
+   short student[20],i;
+   for(i=0;i<20;i++)student[i]=0;
 
    student[1]=10;
    printf("%d %d\n",student[1],student[2]);
